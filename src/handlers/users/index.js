c2c2e9527c2b433d9775647f72a345dda5597f52
Marcelo Vidal <marcelojaviervidal@gmail.com>
2021-06-10 07:46:13 -0300

    Update index.js

--- src/handlers/users/index.js
+++ src/handlers/users/index.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const list = require("./list");
-const find = require("./find");
-const add = require("./add");
-const update = require("./update");
-const remove = require("./remove");
+const usersRouting = require("./users");
+const tasksRouting = require("./tasks");
 
-const usersRouting = express.Router();
-list(usersRouting);
-find(usersRouting);
-add(usersRouting);
-update(usersRouting);
-remove(usersRouting);
+const apiRouting = express.Router();
 
-const usersAPI = express.Router();
-usersAPI.use("/users", usersRouting);
+apiRouting.use("/api", usersRouting);
+apiRouting.use("/api", tasksRouting);
 
-module.exports = usersAPI;
+module.exports = apiRouting;
