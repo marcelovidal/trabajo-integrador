5b6247f3a13a77b3aef93e8b83eb5278b6103e03
Marcelo Vidal <marcelojaviervidal@gmail.com>
2021-06-10 07:51:06 -0300

    sin uso

--- src/validations/user/validateAge.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-module.exports = body('age')
-  .notEmpty()
-  .withMessage('Campo obligatorio')
-  .bail()
-  .isInt()
-  .withMessage('Debe ser un número entero');
