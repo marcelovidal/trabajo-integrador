86323b6b7b17167300a15d31ec5fca719454acc6
Marcelo Vidal <marcelojaviervidal@gmail.com>
2021-06-10 07:48:46 -0300

    Update list.js

--- src/handlers/users/list.js
+++ src/handlers/users/list.js
@@ -1,10 +1,10 @@
-const database = require('../../database');
+const users = require("../../db/users");
 
 // GET /api/users
+
 module.exports = (route) => {
-  route.get('/', async (req, res) => {
-    const filterName = req.query.filterName;
-    const users = await database.list(filterName);
-    res.json(users);
+  route.get("/", async (req, res) => {
+    const usersList = await users.list();
+    res.json(usersList);
   });
 };
